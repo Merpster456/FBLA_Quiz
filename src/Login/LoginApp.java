7fc0195a3406702f49fab0ef06646a5d538cc545
Merpster <ryan.quirk6@protonmail.ch>
2021-02-15 16:44:21 -0500

    Creating starting screen.

---
+++ src/Login/LoginApp.java
@@ -0,0 +1,31 @@
+package Login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * Starts the application
+ */
+public class LoginApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.initStyle(StageStyle.DECORATED);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
