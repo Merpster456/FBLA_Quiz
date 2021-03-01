3913bf8bcb495b4287b7fc399d6e67dfcd75d5cb
Merpster <ryan.quirk6@protonmail.ch>
2021-03-01 18:29:36 -0500

    Clean up

--- src/Start/StartController.java
+++ src/Start/StartController.java
@@ -38,7 +38,7 @@ public class StartController implements Initializable {
 
         stage.close();
         Stage nextStage = new Stage();
-        Pane root = FXMLLoader.load(getClass().getResource("/Start/Login/Login.FXML"));
+        Pane root = FXMLLoader.load(getClass().getResource("/Start/Login/Login.fxml"));
         Scene scene = new Scene(root);
         nextStage.setResizable(false);
         nextStage.setTitle("Login");
